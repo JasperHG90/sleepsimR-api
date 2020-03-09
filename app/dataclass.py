dabcafad1c83407a44d91fe974a57390f47d6c70
Jasper Ginn <jasperginn@gmail.com>
2020-03-09 10:52:32 +0100

    Add data class for keeping track of simulations

--- app/dataclass.py
+++ app/dataclass.py
@@ -6,61 +6,88 @@ class SimulationData:
 
     def __init__(self):
         # Load CSV with scenarios
-        # ...
-        # Load json file with allocations 
-        # ...
-        # (pod_id: {iteration_id, finished: True/False})
-        # On startup --> all pods that are not on finished are removed from overview
-        # ...
-        # Invert index
-        # {iteration_id: pod_id}
-        # ...
-        None
+        self.scen = pd.read_csv("data/scenarios.csv.gz")
+        self.scen["allocated"] = False
+        self.scen.astype("bool")
+        # Set up dict for allocations
+        self.allocations = {}
+        self.allocations_inv = {}
 
     def allocate(self, container_id: str):
         """
         Allocate a container to a set of simulation parameters
         """
-        None
+        params = self.scen[self.scen.allocated == False].sample(n=1)
+        # Set allocation to true
+        self.scen.loc[self.scen["scen.iteration_id"] == params.iteration_id.values[0], "allocated"] = True
+        # Save in allocations
+        self.allocations[container_id] = {"iteration_id": params.iteration_id.values[0],
+                                          "status": "working"}
+        # Save in inverse mapping
+        self.allocations_inv[params.iteration_id.values[0]] = container_id
+        # Save allocations to disk
+        self.save_allocations()
+        # Return values as dict
+        par_return = params.to_dict(orient = "list")
+        # Set json columns to json
+        par_return["start_gamma"] = json.loads(par_return["start_gamma"][0])
+        par_return["start_emiss"] = json.loads(par_return["start_emiss"][0])
+        return par_return
 
     def save_allocations(self):
         """
         Save the current overview of allocations to disk
         """
-        None 
+        with open("allocations.json", "w") as outFile:
+            json.dump({"allocations": self.allocations,
+                       "allocations_inv": self.allocations_inv},
+                       outFile,
+                       indent=4) 
 
     def load_allocations(self):
         """
         Load the allocation from disk
         """
-        None
-
-    def load_scenarios(self):
-        """
-        Load the scenarios from disk
-        """
-        None
+        with open("allocations.json", "r") as inFile:
+            all_ok = json.load(inFile)
+            self.allocations = all_ok["allocations"]
+            self.allocations_inv = all_ok["allocations_inv"]
 
     def pop_allocation(self, container_id: str):
         """
         Remove an allocation from the allocation overview
         """
-        None
+        # Get iteration id
+        itid = self.allocations[container_id]["iteration_id"]
+        # Pop from allocations and inverse
+        del self.allocations[container_id]
+        del self.allocations_inv[itid]
 
     def update_status(self, container_id: str, status = "completed"):
         """
         Update status for a container
         """
-        None
-        # If status is not completed, then pop it
+        self.allocations[container_id]["status"] = status
+        self.save_allocations()
 
     @classmethod
     def from_file(cls):
         """
         Load an allocation from file
         """
-        None
-
-    
-
-    
+        # Set up the class
+        cls_init = cls()
+        # Load allocations
+        cls_init.load_allocations()
+        # Check which allocations are still open
+        # Pop these from the stack
+        for k in list(cls_init.allocations.keys()):
+            if cls_init.allocations[k]["status"] == "working":
+                cls_init.pop_allocation(k)
+            else:
+                # If status is completed, then set pandas allocated column to true
+                cls_init.scen.loc[cls_init.scen["iteration_id"] == cls_init.allocations[k]["iteration_id"], "allocated"] = True
+        # Save allocations
+        cls_init.save_allocations()
+        # Return the updated class
+        return cls_init
--- app/main.py
+++ app/main.py
@@ -4,10 +4,16 @@
 import logging
 import daiquiri
 import sys
+import os
 
 # Import schemas
-from endpoints import app
 from dataclass import SimulationData
+
+# If simulation data exists, load. Else start new
+if "allocations.json" in os.listdir():
+    sd = SimulationData.from_file()
+else:
+    sd = SimulationData()
 
 # Set up daiquiri
 daiquiri.setup(level=logging.INFO)
@@ -22,3 +28,48 @@ daiquiri.setup(level=logging.INFO, outputs=(
     ))
 # Emit
 logger.info("Started simulation API ...")
+
+#################
+# API endpoints #
+#################
+
+# Import modules
+from fastapi import FastAPI
+from schemas import Parameter_req, Simulation_res
+# Create the api
+app = FastAPI()
+
+# GET request for parameters
+@app.get("/parameters")
+def get_parameters(records: Parameter_req):
+    # Get the id of the container that is simulating this iteration
+    cont_id = records.uid
+    # Get a set of parameters
+    out = sd.allocate(cont_id)
+    # Return
+    return out
+
+# POST request for simulation results
+@app.post('/results')
+def save_results(records: Simulation_res):
+    """
+    Take simulation results and save to disk
+    """
+    # Get results
+    res = {
+        "container_uid": records.uid,
+        "scenario_uid": records.scenario_uid,
+        "emiss_mu_bar": records.emiss_mu_bar,
+        "gamma_int_bar": records.gamma_int_bar,
+        "emiss_var_bar": records.emiss_var_bar,
+        "emiss_varmu_bar": records.emiss_varmu_bar,
+        "credible_intervals": records.credible_intervals
+    }
+    # Save results
+    out_file = os.path.join("/var/results", records.iteration_uid + ".json")
+    # ...
+    # Update status
+    sd.update_status(records.uid, status = "completed")
+
+    # Return termination message
+    return({"message":"terminate"})
